21d1dce654f4ac508eb5fd4a115c5914c17f47d3
MankoMax <fanatscape@gmail.com>
2021-12-22 16:11:27 +0200

    Added Methods

--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -1,8 +1,140 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 public static class Program
 {
+    public static void PrintNumbersList(List<string> numbers)
+    {
+        if (numbers.Count > 0)
+        {
+            Console.WriteLine("List of all numbers: \n");
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{numbers.IndexOf(number) + 1}) {number};");
+            }
+        }
+
+        else
+        {
+            Console.WriteLine("There are no numbers!");
+        }
+
+    }
+    public static List<string> AddNumberWithValidations(List<string> numbers)
+    {
+        var inputYN = string.Empty;
+
+        do
+        {
+            Console.Clear();
+
+            Console.WriteLine("Enter the number in the format ХХХ-ХХХ-ХХХ: ");
+
+            var number = Console.ReadLine();
+
+            var numberWithoutSpecialCharacters = number.Replace("+", "").Replace("-", "").Replace(":", "").Replace("(", "").Replace(")", "").Replace(" ", "");
+
+            var arrayOfNumberSymbols = numberWithoutSpecialCharacters.ToCharArray();
+
+
+            if (numberWithoutSpecialCharacters.Length != 9)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Invalid number of characters! \n\nDo you want to continue y/n?");
+
+                inputYN = Console.ReadLine();
+
+                continue;
+            }
+
+            var numberContainsNonDigitCharacters = false;
+
+            foreach (var numberSymbol in arrayOfNumberSymbols)
+            {
+                if (!char.IsDigit(numberSymbol))
+                {
+                    numberContainsNonDigitCharacters = true;
+
+                    break;
+                }
+            }
+
+            if (numberContainsNonDigitCharacters)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Invalid input characters!\n\nDo you want to continue y/n?");
+
+                inputYN = Console.ReadLine();
+
+                continue;
+            }
+            numbers.Add(numberWithoutSpecialCharacters);
+
+            break;
+        }
+        while (inputYN == "y");
+
+        return numbers;
+    }
+    public static List<string> DeleteNumberWithValidations(List<string> numbers)
+    {
+        var inputYN = string.Empty;
+
+        if (numbers.Count > 0)
+        {
+            var isExceptionOccured = false;
+
+            do
+            {
+                try
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("Which of the numbers should you delete?: \n");
+
+                    foreach (var number in numbers)
+                    {
+                        Console.WriteLine($"{numbers.IndexOf(number) + 1}) {number};");
+                    }
+
+                    Console.WriteLine("\nEnter the sequence number: > ");
+
+                    var index = int.Parse(Console.ReadLine());
+
+                    numbers.RemoveAt(index - 1);
+
+                    isExceptionOccured = false;
+
+                }
+                catch
+                {
+                    isExceptionOccured = true;
+
+                    Console.Clear();
+
+                    Console.WriteLine("Invalid input!\n\nDo you want to continue y/n?");
+
+                    inputYN = Console.ReadLine();
+                }
+            }
+            while (isExceptionOccured && inputYN == "y");
+
+        }
+        else
+        {
+            Console.WriteLine("There are no numbers!");
+
+            Console.ReadKey();
+        }
+
+        return numbers;
+    }
+
+    
     public static void Main()
     {
         var numbers = new List<string>();
@@ -28,57 +160,7 @@ public static class Program
                 case "1":
                     Console.Clear();
 
-                    do
-                    {
-                        Console.Clear();
-
-                        Console.WriteLine("Enter the number in the format ХХХ-ХХХ-ХХХ: ");
-
-                        var number = Console.ReadLine();
-
-                        var numberWithoutSpecialCharacters = number.Replace("+", "").Replace("-", "").Replace(":", "").Replace("(", "").Replace(")", "").Replace(" ", "");
-
-                        var arrayOfNumberSymbols = numberWithoutSpecialCharacters.ToCharArray();
-
-
-                        if (numberWithoutSpecialCharacters.Length != 9)
-                        {
-                            Console.Clear();
-
-                            Console.WriteLine("Invalid number of characters! \n\nDo you want to continue y/n?");
-
-                            inputYN = Console.ReadLine();
-
-                            continue;
-                        }
-
-                        var numberContainsNonDigitCharacters = false;
-
-                        foreach (var numberSymbol in arrayOfNumberSymbols)
-                        {
-                            if (!char.IsDigit(numberSymbol))
-                            {
-                                numberContainsNonDigitCharacters = true;
-
-                                break;
-                            }
-                        }
-
-                        if (numberContainsNonDigitCharacters)
-                        {
-                            Console.Clear();
-
-                            Console.WriteLine("Invalid input characters!\n\nDo you want to continue y/n?");
-
-                            inputYN = Console.ReadLine();
-
-                            continue;
-                        }
-                        numbers.Add(numberWithoutSpecialCharacters);
-
-                        break;
-                    }
-                    while (inputYN == "y");
+                    AddNumberWithValidations(numbers);
 
                     break;
 
@@ -86,52 +168,7 @@ public static class Program
 
                     Console.Clear();
 
-                    if (numbers.Count > 0)
-                    {
-                        var isExceptionOccured = false;
-
-                        do
-                        {
-                            try
-                            {
-                                Console.Clear();
-
-                                Console.WriteLine("Which of the numbers should you delete?: \n");
-
-                                foreach (var number in numbers)
-                                {
-                                    Console.WriteLine($"{numbers.IndexOf(number) + 1}) {number};");
-                                }
-
-                                Console.WriteLine("\nEnter the sequence number: > ");
-
-                                var index = int.Parse(Console.ReadLine());
-
-                                numbers.RemoveAt(index - 1);
-
-                                isExceptionOccured = false;
-
-                            }
-                            catch
-                            {
-                                isExceptionOccured = true;
-
-                                Console.Clear();
-
-                                Console.WriteLine("Invalid input!\n\nDo you want to continue y/n?");
-
-                                inputYN = Console.ReadLine();
-                            }
-                        }
-                        while (isExceptionOccured && inputYN == "y");
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("There are no numbers!");
-
-                        Console.ReadKey();
-                    }
+                    DeleteNumberWithValidations(numbers);
 
                     break;
 
@@ -151,10 +188,7 @@ public static class Program
 
                                 Console.WriteLine("Which of the numbers should you edit?: \n");
 
-                                foreach (var number in numbers)
-                                {
-                                    Console.WriteLine($"{numbers.IndexOf(number) + 1}) {number};");
-                                }
+                                PrintNumbersList(numbers);
 
                                 Console.WriteLine("\nEnter the sequence number: > ");
 
@@ -162,59 +196,10 @@ public static class Program
 
                                 numbers.RemoveAt(index - 1);
 
-                                do
-                                {
-                                    Console.Clear();
-
-                                    Console.WriteLine("Enter the number in the format ХХХ-ХХХ-ХХХ: ");
-
-                                    var number = Console.ReadLine();
-
-                                    var numberWithoutSpecialCharacters = number.Replace("+", "").Replace("-", "").Replace(":", "").Replace("(", "").Replace(")", "").Replace(" ", "");
-
-                                    var arrayOfNumberSymbols = numberWithoutSpecialCharacters.ToCharArray();
-
-
-                                    if (numberWithoutSpecialCharacters.Length != 9)
-                                    {
-                                        Console.Clear();
-
-                                        Console.WriteLine("Invalid number of characters! \n\nDo you want to continue y/n?");
-
-                                        inputYN = Console.ReadLine();
-
-                                        continue;
-                                    }
-
-                                    var numberContainsNonDigitCharacters = false;
-
-                                    foreach (var numberSymbol in arrayOfNumberSymbols)
-                                    {
-                                        if (!char.IsDigit(numberSymbol))
-                                        {
-                                            numberContainsNonDigitCharacters = true;
-
-                                            break;
-                                        }
-                                    }
-
-                                    if (numberContainsNonDigitCharacters)
-                                    {
-                                        Console.Clear();
-
-                                        Console.WriteLine("Invalid input characters!\n\nDo you want to continue y/n?");
-
-                                        inputYN = Console.ReadLine();
-
-                                        continue;
-                                    }
-                                    numbers.Add(numberWithoutSpecialCharacters);
-
-                                    break;
-                                }
-                                while (inputYN == "y");
+                                AddNumberWithValidations(numbers);
 
                                 break;
+
 
                                 isExceptionOccured = false;
 
@@ -245,19 +230,7 @@ public static class Program
                 case "4":
                     Console.Clear();
 
-                    if (numbers.Count > 0)
-                    {
-                        Console.WriteLine("List of all numbers: \n");
-
-                        foreach (var number in numbers)
-                        {
-                            Console.WriteLine($"{numbers.IndexOf(number) + 1}) {number};");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("There are no numbers!");
-                    }
+                    PrintNumbersList(numbers);
 
                     Console.ReadKey();
 
