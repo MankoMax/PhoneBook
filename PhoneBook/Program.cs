5397b7b9440a3afe48e03429c0bbe7983de7a512
MankoMax <fanatscape@gmail.com>
2022-01-13 11:37:38 +0200

    Added new features

--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -1,212 +1,258 @@
-﻿using System;
+﻿using PhoneBook;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 public static class Program
 {
-    public static void PrintNumbersList(List<string> numbers)
-    {
-        if (numbers.Count > 0)
-        {
-            Console.WriteLine("List of all numbers: \n");
+    public static List<Record> _numbers = new List<Record>();
 
-            foreach (var number in numbers)
-            {
-                Console.WriteLine($"{numbers.IndexOf(number) + 1}) {number};");
-            }
-        }
-
-        else
-        {
-            Console.WriteLine("There are no numbers!");
-        }
-
-    }
-    public static List<string> AddNumberWithValidations(List<string> numbers)
-    {
-        var inputYN = string.Empty;
-
-        do
-        {
-            Console.Clear();
-
-            Console.WriteLine("Enter the number in the format ХХХ-ХХХ-ХХХ: ");
-
-            var number = Console.ReadLine();
-
-            var numberWithoutSpecialCharacters = number.Replace("+", "").Replace("-", "").Replace(":", "").Replace("(", "").Replace(")", "").Replace(" ", "");
-
-            var arrayOfNumberSymbols = numberWithoutSpecialCharacters.ToCharArray();
-
-
-            if (numberWithoutSpecialCharacters.Length != 9)
-            {
-                Console.Clear();
-
-                Console.WriteLine("Invalid number of characters! \n\nDo you want to continue y/n?");
-
-                inputYN = Console.ReadLine();
-
-                continue;
-            }
-
-            var numberContainsNonDigitCharacters = false;
-
-            foreach (var numberSymbol in arrayOfNumberSymbols)
-            {
-                if (!char.IsDigit(numberSymbol))
-                {
-                    numberContainsNonDigitCharacters = true;
-
-                    break;
-                }
-            }
-
-            if (numberContainsNonDigitCharacters)
-            {
-                Console.Clear();
-
-                Console.WriteLine("Invalid input characters!\n\nDo you want to continue y/n?");
-
-                inputYN = Console.ReadLine();
-
-                continue;
-            }
-            numbers.Add(numberWithoutSpecialCharacters);
-
-            break;
-        }
-        while (inputYN == "y");
-
-        return numbers;
-    }
-    public static List<string> DeleteNumberWithValidations(List<string> numbers)
-    {
-        var inputYN = string.Empty;
-
-        if (numbers.Count > 0)
-        {
-            var isExceptionOccured = false;
-
-            do
-            {
-                try
-                {
-                    Console.Clear();
-
-                    Console.WriteLine("Which of the numbers should you delete?: \n");
-
-                    foreach (var number in numbers)
-                    {
-                        Console.WriteLine($"{numbers.IndexOf(number) + 1}) {number};");
-                    }
-
-                    Console.WriteLine("\nEnter the sequence number: > ");
-
-                    var index = int.Parse(Console.ReadLine());
-
-                    numbers.RemoveAt(index - 1);
-
-                    isExceptionOccured = false;
-
-                }
-                catch
-                {
-                    isExceptionOccured = true;
-
-                    Console.Clear();
-
-                    Console.WriteLine("Invalid input!\n\nDo you want to continue y/n?");
-
-                    inputYN = Console.ReadLine();
-                }
-            }
-            while (isExceptionOccured && inputYN == "y");
-
-        }
-        else
-        {
-            Console.WriteLine("There are no numbers!");
-
-            Console.ReadKey();
-        }
-
-        return numbers;
-    }
-
-    
     public static void Main()
     {
-        var numbers = new List<string>();
-
-        var input = string.Empty;
-
-        var inputYN = string.Empty;
-
         while (true)
         {
-            Console.Clear();
-            Console.WriteLine("1 - Add new number");
-            Console.WriteLine("2 - Delete one of the numbers");
-            Console.WriteLine("3 - Edit one of the numbers");
-            Console.WriteLine("4 - List of all numbers");
-            Console.WriteLine("\n5 - Exit the program");
-            Console.Write("\nType the command and press Enter: > ");
+            Clear();
 
-            input = Console.ReadLine();
-
-            switch (input)
+            switch (ShowMenu())
             {
                 case "1":
-                    Console.Clear();
-
-                    AddNumberWithValidations(numbers);
+                    AddNumberWithValidations();
 
                     break;
 
                 case "2":
-
-                    Console.Clear();
-
-                    DeleteNumberWithValidations(numbers);
+                    DeleteNumberWithValidations();
 
                     break;
 
                 case "3":
-
-                    Console.Clear();
-
-                    DeleteNumberWithValidations(numbers);
-
-                    AddNumberWithValidations(numbers);
+                    EditNumbers();
 
                     break;
 
                 case "4":
-                    Console.Clear();
-
-                    PrintNumbersList(numbers);
+                    PrintNumbersUsingCountCheck();
 
                     Console.ReadKey();
 
                     break;
 
                 case "5":
-
-                    Console.Clear();
-
                     Environment.Exit(0);
 
                     break;
 
                 default:
-
-                    Console.Clear();
+                    Clear();
 
                     break;
             }
         }
+    }
 
+    public static bool Validate(string number, out string message)
+    {
+        if (number.Length != 9)
+        {
+            message = "Invalid number of characters!";
+
+            return false;
+        }
+
+        var arrayOfNumberSymbols = number.ToCharArray();
+
+        foreach (var numberSymbol in arrayOfNumberSymbols)
+        {
+            if (!char.IsDigit(numberSymbol))
+            {
+                message = "Invalid input characters!";
+
+                return false;
+            }
+        }
+
+        message = string.Empty;
+
+        return true;
+    }
+
+    public static void Clear()
+    {
+        Console.Clear();
+    }
+
+    public static string ShowMenu()
+    {
+        Clear();
+
+        Console.WriteLine("1 - Add new number");
+        Console.WriteLine("2 - Delete one of the numbers");
+        Console.WriteLine("3 - Edit one of the numbers");
+        Console.WriteLine("4 - List of all numbers");
+        Console.WriteLine("\n5 - Exit the program");
+
+        return GetDataFromUser("\nType the command and press Enter : > ");
+    }
+
+    public static void EditNumbers()
+    {
+        Clear();
+
+        if (_numbers.Count > 0)
+        {
+            DeleteNumberWithValidations();
+            AddNumberWithValidations();
+        }
+        else
+        {
+            Console.WriteLine("There are no numbers!");
+            Console.ReadKey();
+        }
+    }
+
+    public static string GetDataFromUser(string messageToShow, bool shouldAddNewLine = false)
+    {
+        if (shouldAddNewLine)
+        {
+            Console.WriteLine(messageToShow);
+        }
+        else
+        {
+            Console.Write(messageToShow);
+        }
+
+        var userResult = Console.ReadLine();
+
+        return userResult;
+    }
+
+    public static void PrintNumbers()
+    {
+        Clear();
+
+        Console.WriteLine("List of all numbers: \n");
+
+        foreach (var number in _numbers)
+        {
+            Console.WriteLine($"{_numbers.IndexOf(number) + 1}) Name: {number._name}; \n  Number: {number._number}\n");
+        }
+
+    }
+
+    public static void PrintNumbersUsingCountCheck()
+    {
+        Clear();
+
+        if (_numbers.Count > 0)
+        {
+            PrintNumbers();
+        }
+        else
+        {
+            Console.WriteLine("There are no numbers!");
+        }
+    }
+
+    public static string ReplaceSymbols(string number)
+    {
+        var regEx = new Regex(" *[\\~#%&*{}()/:<>?|\"-]+ *");
+        var numberWithoutSpecialCharacters = regEx.Replace(number, string.Empty);
+
+        return numberWithoutSpecialCharacters;
+    }
+
+    public static void AddNumberWithValidations()
+    {
+        var inputYN = string.Empty;
+
+        do
+        {
+            Clear();
+
+            var name = GetDataFromUser("Enter the name: ", true);
+            var number = GetDataFromUser("\nEnter the number in the format ХХХ-ХХХ-ХХХ: ", true);
+            var numberWithoutSpecialChars = ReplaceSymbols(number);
+            var record = new Record(numberWithoutSpecialChars, name);
+            var isNumberValid = Validate(numberWithoutSpecialChars, out var message);
+
+            if (!isNumberValid)
+            {
+                Clear();
+
+                Console.WriteLine(message);
+
+                inputYN = GetDataFromUser("\n\nDo you want to continue y/n? : > ");
+            }
+            else
+            {
+                _numbers.Add(record);
+            }
+        }
+        while (inputYN == "y");
+
+    }
+
+    public static void DeleteNumberWithValidations()
+    {
+        if (_numbers.Count > 0)
+        {
+            var inputYN = string.Empty;
+
+            do
+            {
+                var isDeleted = TryToDelete(_numbers, out var message);
+
+                if (isDeleted)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(message);
+
+                    inputYN = GetDataFromUser("\n\nDo you want to continue y/n? : > ");
+                }
+
+            }
+            while (inputYN == "y");
+
+        }
+        else
+        {
+            Clear();
+
+            Console.WriteLine("There are no numbers!");
+            Console.ReadKey();
+        }
+    }
+
+    public static bool TryToDelete(List<Record> _numbers, out string message)
+    {
+        Clear();
+
+        try
+        {
+            Console.Write("Which of the numbers should you delete?:\n");
+
+            PrintNumbers();
+
+            var index = int.Parse(GetDataFromUser("\nEnter the sequence number : > "));
+
+            _numbers.RemoveAt(index - 1);
+
+            message = string.Empty;
+
+            return true;
+        }
+        catch (Exception exception)
+        {
+            Clear();
+
+            message = exception.Message;
+
+            return false;
+        }
     }
 }
 
---
+++ PhoneBook/Record.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    public class Record
+    {
+        public string _number;
+        public string _name;
+
+        public Record(string number, string name)
+        {
+            _number = number;
+            _name = name;
+        }
+    }
+}
